dcea17844979bd02f0753706facfa2fb625ebd80
Abhishek Pathak <pathakabhishek0003@gmail.com>
2019-02-04 21:39:05 +0530

    Basic template C code is created

---
+++ firstcome_firstserve.c
@@ -0,0 +1,17 @@
+// This is a CPU Scheduling Algorithm
+
+#include <stdio.h>
+
+void main(){
+
+    int process;
+
+    //arrays
+    int process_arrival_time[50], process_burst_time[50];
+
+    printf("Enter the no. of processes");
+    scanf("%d", &process);
+
+    
+
+}
